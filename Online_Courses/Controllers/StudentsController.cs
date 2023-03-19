080a909d8f8cf5bf1694ee84b3d906747591130c
Zomorod <Zomorod@DESKTOP-I6PEBO9>
2023-03-19 12:26:40 +0330

    Add project files.

---
+++ Online_Courses/Controllers/StudentsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Online_Courses.Models;
+
+namespace Online_Courses.Controllers
+{
+    public class StudentsController : Controller
+    {
+        db_EducationEntities db = new db_EducationEntities();
+
+        public ActionResult Index()
+        {
+            var student= db.T_Students;
+            return View(student);
+        }
+    }
+}
